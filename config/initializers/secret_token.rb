1b41a11f61001337cd7ef92906034b4361110598
kozyty <kozyty@gmail.com>
2013-12-23 20:51:59 +0900

    rails new rails4admin --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4admin::Application.config.secret_key_base = '97336b9f13dbfd1f9c1f688f2a20802dd92c64414e282aebc3311a32590b5ffe62de163b8184fbc100a869d3059f7c3404472d5c200613fbd4169aed9bc0f31f'
